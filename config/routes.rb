0776519b93a933fbe53aae30bd831a606e9ffcc1
Benjamin Lemaire <bn.lemaire@gmail.com>
2016-03-15 17:40:32 +0100

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'meals#index'
+
+  resources :users do
+    ressources :profiles
+  end
+
+  ressources :restaurants do
+    ressources :meals, only: [:new, :create, :update, :edit, :destroy]
+  end
+
+  resources :meals, only [:index, :show] do
+    ressources :reviews
+  end
+
+  ressources :orders
+
 end

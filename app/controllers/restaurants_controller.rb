7c37a20e4e852e1e83b27b4468e1f8f87c3add4b
Augustin Mulliez <mulliez.augustin@gmail.com>
2016-03-15 20:49:28 +0100

    we can index show edit delete restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,14 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, [:show, :edit, :update, :destroy]
+
+  def index
+  end
 
   def new
     @restaurant = Restaurant.new
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
     @user = current_user
   end
 
@@ -20,7 +23,27 @@ class RestaurantsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
 private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 
   def restaurant_params
     params.require(:restaurant).permit( :name,

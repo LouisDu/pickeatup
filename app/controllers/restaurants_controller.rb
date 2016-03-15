1cb6430360195bb4e63dfec060e43bc311f6e227
Augustin Mulliez <mulliez.augustin@gmail.com>
2016-03-15 15:51:34 +0100

    implement controller for meal and restaurant

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,37 @@
+class RestaurantsController < ApplicationController
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def show
+    @place = Place.find(params[:id])
+    @user = current_user
+  end
+
+  def create
+    @user = current_user
+    @restaurant = @user.restaurants.build(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render :new
+    end
+  end
+
+private
+
+  def restaurant_params
+    params.require(:restaurant).permit( :name,
+                                        :street_number,
+                                        :route,
+                                        :locality,
+                                        :administrative_area_level_1,
+                                        :postal_code,
+                                        :country,
+                                        :logo,
+                                        :logo_cache,
+                                        :restaurant_type_id,
+                                        :user_id )
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,11 @@ if Rails.env.development?
   User.destroy_all
 end
 
-user = User.new(email: mulliez.augustin@gmail.com, password: 'pickeatup')
+user = User.new(email: "mulliez.augustin@gmail.com", password: 'pickeatup')
 user.save
-user = User.new(email: louis.dublancq@gmail.com, password: 'pickeatup')
+user = User.new(email: "louis.dublancq@gmail.com", password: 'pickeatup')
 user.save
-user = User.new(email: bn.lemaire@gmail.com, password: 'pickeatup')
+user = User.new(email: "bn.lemaire@gmail.com", password: 'pickeatup')
 user.save
-user = User.new(email: guillaume@fortaine.com, password: 'pickeatup')
+user = User.new(email: "guillaume@fortaine.com", password: 'pickeatup')
 user.save

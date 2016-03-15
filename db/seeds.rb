c2fed965efaae12f5bdf86031a2a5649d8f6e7c6
Louis Dublancq <louis.dublancq@gmail.com>
2016-03-15 18:04:46 +0100

    Seeds for types

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
-if Rails.env.development?
-  User.destroy_all
-end
+User.destroy_all
 
 user = User.new(email: "mulliez.augustin@gmail.com", password: 'pickeatup')
 user.save
@@ -10,3 +8,25 @@ user = User.new(email: "bn.lemaire@gmail.com", password: 'pickeatup')
 user.save
 user = User.new(email: "guillaume@fortaine.com", password: 'pickeatup')
 user.save
+
+RestaurantType.create!(name: "Italien")
+RestaurantType.create!(name: "Americain")
+RestaurantType.create!(name: "Thai")
+RestaurantType.create!(name: "Japonais")
+RestaurantType.create!(name: "Français")
+RestaurantType.create!(name: "Mexicain")
+RestaurantType.create!(name: "Marocain")
+RestaurantType.create!(name: "Argentin")
+RestaurantType.create!(name: "Chinois")
+RestaurantType.create!(name: "Libanais")
+RestaurantType.create!(name: "Turque")
+RestaurantType.create!(name: "Espagnol")
+RestaurantType.create!(name: "Russe")
+RestaurantType.create!(name: "Vietnamien")
+RestaurantType.create!(name: "Ethiopien")
+
+MealType.create!(name: "Entrées")
+MealType.create!(name: "Plats")
+MealType.create!(name: "Desserts")
+MealType.create!(name: "Boissons")
+MealType.create!(name: "Menus")

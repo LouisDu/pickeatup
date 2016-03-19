cee33bfd98404ec81e43630d71d66014184a6518
Benjamin Lemaire <bn.lemaire@gmail.com>
2016-03-19 15:57:11 +0100

    in progress button review on meal

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,15 @@
 class ReviewsController < ApplicationController
 
+  def index
+    @meal = Meal.find(params[:meal_id])
+    @reviews = policy_scope(Review)
+    @review = Review.new
+  end
+
+# Faire une methode pour calculer la note moyenne. Ensuite tu removes le button "review"
+# pour afficher la note qui est linkee jusqu'aux summarize comment.
+# PLes review + le form doivent s afficher.
+#
   def create
     @meal = Meal.find(params[:meal_id])
     @review = @meal.reviews.build(review_params)
@@ -14,6 +24,13 @@ class ReviewsController < ApplicationController
       render 'meals/show'
     end
   end
+
+  # def average_rating
+  # @reviews = self.reviews
+  # @review_sum = reviews.inject(0) { |sum, review| sum += review.rating }
+  # avg_rating = (review_sum / reviews.count).to_i unless review_sum == 0
+  # end
+
 
 private
   def review_params
